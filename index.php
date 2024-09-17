ede2f1e25407f9f8fd1edd1fbd618ab84684ab4b
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-09-17 15:56:47 +0200

    [UPD] defined the constructor

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
 <?php 
+// definisco la classe MovieInfo per utilizzarla con construct 
+class MovieInfo {
+    public $description;
+    public $genre;
+    public $year;
+    public $director;
+    function __construct($description, $genre, $year, $director) { 
+        $this->description = $description;
+        $this->genre = $genre;
+        $this->year = $year;
+        $this->director = $director; 
+    }
+}
+
 // definisco la classe movie 
 class Movie {
     // dichiaro le varibili d'istanza
     public $title;
     public $cover;
-    public $description;
-    public $genre;
-    public $year;
-    public $director;
 
+    public $info;
 
-
+    // definisco un costruttore
+    public function __construct($_title, $_cover,MovieInfo $_info) {
+        $this->title = $_title;
+        $this->cover = $_cover;
+        $this->title = $_info;
+    }
 }
 ?>
