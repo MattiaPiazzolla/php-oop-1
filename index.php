97d98c8e43fe81498f6059334cf1e013ac7095fd
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-09-17 15:14:34 +0200

    [UPD] defined class variable

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php 
 // definisco la classe movie 
 class Movie {
-    
+    // dichiaro le varibili d'istanza
+    public $title;
+    public $cover;
+    public $description;
+    public $genre;
+    public $year;
+    public $director;
+
+
+
 }
 ?>
